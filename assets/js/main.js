e6d0b7a088360296843d97754c4ddf37a9f77b0d
Allison King <allisonjuliaking@gmail.com>
2017-01-18 20:18:56 -0500

    adding scrolling and fixed background image

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,6 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-$("#engineering_button").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#engineering").offset().top
-    }, 700);
-});
-
-$("#hyphen_button").click(function() {
-	$('html, body').animate({
-		scrollTop: $("#hyphen").offset().top
-	}, 700);
-});
-
-$("#writing_button").click(function() {
-	$('html, body').animate({
-		scrollTop: $("#writing").offset().top
-	}, 700);
-});
-
 (function($) {
 
 	var settings = {
