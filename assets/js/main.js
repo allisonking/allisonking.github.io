dbe51417dc87f682e08cb4e74c40191fc6ca6423
Allison King <allisonjuliaking@gmail.com>
2017-05-07 19:23:25 -0400

    revise hyperlinks

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,7 +121,7 @@
 						overlayOpacity: 0.85,
 						popupCloserText: '',
 						popupLoaderText: '',
-						selector: '.work-item a.image',
+						selector: '.pop-item a.image',
 						usePopupCaption: true,
 						usePopupDefaultStyling: false,
 						usePopupEasyClose: false,
